a569a0b4e50aa02c901385adfa920b5dd9fc62e3
Jatin Kumar <jatin208015@gmail.com>
2024-09-19 23:28:11 +0530

    hibernate mariadb bug fix

--- hotel-reservation/hotel-reservation-backend/src/main/java/com/jatin/hotel_reservation/repository/RoomRepository.java
+++ hotel-reservation/hotel-reservation-backend/src/main/java/com/jatin/hotel_reservation/repository/RoomRepository.java
@@ -11,6 +11,6 @@ import com.jatin.hotel_reservation.entity.Room;
 
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
-	@Query(value = "SELECT * FROM room r WHERE r.is_available = 1 ", nativeQuery = true)
+	@Query(value = "SELECT * FROM room r WHERE r.is_available = 1 AND r.room_type = :roomType AND r.available_from <= :startDate AND r.available_to >= :endDate", nativeQuery = true)
 	List<Room> findAvailableRooms(LocalDate startDate, LocalDate endDate, String roomType);
 }
